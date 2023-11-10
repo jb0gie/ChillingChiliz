ee80d6d49149ea64961e45e86ea1d8055178928e
j_b0gie <wxzvrd@protonmail.com>
2023-11-10 01:45:07 +0000

    added threlte, started scaffolding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,55 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+	import { ModeWatcher } from 'mode-watcher';
+
+	// SEO
+	const metaDefaults = {
+		title: '3PS',
+		description: 'Concierge/DAO',
+		image: '../static/favicon.png'
+	};
+	const meta = {
+		title: metaDefaults.title,
+		description: metaDefaults.description,
+		image: metaDefaults.image,
+		// Article
+		article: { publishTime: '', modifiedTime: '', author: '' },
+		// Twitter
+		twitter: {
+			title: metaDefaults.title,
+			description: metaDefaults.description,
+			image: metaDefaults.image
+		}
+	};
+</script>
+
+<svelte:head>
+	<title>{meta.title}</title>
+	<meta name="title" content={meta.title} />
+	<meta name="description" content={meta.description} />
+	<meta name="keywords" content={meta.description} />
+	<meta name="author" content="b0gieman" />
+	<meta property="og:site_name" content="3PS" />
+	<meta property="og:type" content="website" />
+	<!-- <meta property="og:url" content="https://thirdplanet.studio{$page.url.pathname}" /> -->
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:title" content={meta.title} />
+	<meta property="og:description" content={meta.description} />
+	<meta property="og:image" content={meta.image} />
+	<meta property="og:image:secure_url" content={meta.image} />
+	<meta property="og:image:type" content="image/jpg" />
+	<meta property="og:image:width" content="" />
+	<!--FIX-->
+	<meta property="og:image:height" content="" />
+	<!--ALSO FIX-->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@3PlanetStudio" />
+	<meta name="twitter:creator" content="@3PlanetStudio" />
+	<meta name="twitter:title" content={meta.twitter.title} />
+	<meta name="twitter:description" content={meta.twitter.description} />
+	<meta name="twitter:image" content={meta.twitter.image} />
+</svelte:head>
+<!-- DarkMode -->
+<ModeWatcher />
+
+<slot />
